c33254d16beb81bba601e3238c23670f37a5d911
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2022-01-10 11:04:08 +0100

    update

--- Streamlit/my_streamlit_app.py
+++ Streamlit/my_streamlit_app.py
@@ -1,16 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
-import pydeck as pdk
 import datetime as dt
 import plotly.express as px
-import ipywidgets as widgets
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 st.header("Streamlit : build and share data apps")
 
@@ -18,4 +12,4 @@ st.header("Streamlit : build and share data apps")
 
 
 
-st.image('assets/challenge.png')
+st.image('Streamlit/assets/challenge.png')
