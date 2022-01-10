99db59d30492a2891fd3d13efb2d3a3842e21ba7
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2022-01-10 11:17:58 +0100

    Update my_streamlit_app.py

--- Streamlit/my_streamlit_app.py
+++ Streamlit/my_streamlit_app.py
@@ -7,9 +7,15 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
 st.header("Streamlit : build and share data apps")
-
-
-
-
-
 st.image('Streamlit/assets/challenge.png')
+
+df_voiture = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
+
+
+import seaborn as sns
+viz_correlation = sns.heatmap(df_voiture.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
